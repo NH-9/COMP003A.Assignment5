738db78b477f67b1e3ed9a878bd3688d10f5e5f0
NH <noahhulett@my.whccd.edu>
2026-02-11 22:21:17 -0800

    user input handling added

--- COMP003A.Assignment5/Program.cs
+++ COMP003A.Assignment5/Program.cs
@@ -7,11 +7,24 @@ namespace COMP003A.Assignment5
         static void Main(string[] args)
         {
             ProgramIntroduction();
+
+            Console.Write("\nEnter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter a number: ");
+            int numA = int.Parse(Console.ReadLine());
+            UserInputHandling(name, numA);
+
+
         }
 
         static void ProgramIntroduction()
         {
             Console.WriteLine("Welcome to the 5th assignment!");
+        }
+
+        static void UserInputHandling(string name, int number)
+        {
+            Console.WriteLine($"Name: {name}, Number: {number}");
         }
     }
 }
