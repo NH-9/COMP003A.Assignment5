33fd54767974e31a4208f91586358cf4bff6bf2d
NH <noahhulett@my.whccd.edu>
2026-02-11 21:19:54 -0800

    Add project files.

---
+++ COMP003A.Assignment5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace COMP003A.Assignment5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
