97983091bbf33771f19c4df0669d3d8f7f4f0f05
NH <noahhulett@my.whccd.edu>
2026-02-11 23:15:47 -0800

    program looping added

--- COMP003A.Assignment5/Program.cs
+++ COMP003A.Assignment5/Program.cs
@@ -6,25 +6,35 @@ namespace COMP003A.Assignment5
     {
         static void Main(string[] args)
         {
-            ProgramIntroduction();
-
-            Console.Write("\nEnter your name: ");
-            string name = Console.ReadLine();
-            Console.Write("Enter a number: ");
-            int numA = int.Parse(Console.ReadLine());
-            UserInputHandling(name, numA);
-
-            int numDouble = CalculationLogic(numA);
-            Console.WriteLine($"\n{numA} doubled is {numDouble}");
-
-            if ( numDouble > 50)
+            bool keepGoing = false;
+            do
             {
-                Console.WriteLine($"{numDouble} is greater than 50");
+                ProgramIntroduction();
+
+                Console.Write("\nEnter your name: ");
+                string name = Console.ReadLine();
+                Console.Write("Enter a number: ");
+                int numA = int.Parse(Console.ReadLine());
+                UserInputHandling(name, numA);
+
+                int numDouble = CalculationLogic(numA);
+                Console.WriteLine($"\n{numA} doubled is {numDouble}");
+
+                if (numDouble > 50)
+                {
+                    Console.WriteLine($"{numDouble} is greater than 50");
+                }
+                else
+                {
+                    Console.WriteLine($"{numDouble} is not greater than 50");
+                }
+
+                Console.Write("\nWould you like to run this program again? (y/n): ");
+                keepGoing = Console.ReadLine().ToLower() == "y";
+
+                Console.WriteLine();
             }
-            else
-            {
-                Console.WriteLine($"{numDouble} is not greater than 50");
-            }
+            while (keepGoing);
         }
 
         static void ProgramIntroduction()
